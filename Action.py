4a8cb50316f37d2f57c4d53763695cb1bb6f4256
Harper Zhang <zuii@qq.com>
2014-06-25 01:58:16 +0800

    add some basic files

---
+++ Action.py
@@ -0,0 +1,36 @@
+#
+# Actions
+#
+
+def ActionFactory():
+	''' action factory, generate actions object'''
+	pass
+
+#
+# Action classes
+#
+
+# Action base class
+class Action
+	def __init__(self):
+		pass
+
+# Action: get new ip
+class GetIP : Action
+	def __init__(self):
+		pass
+
+# Action: refresh verify code
+class RefreshVerfyCode : Action
+	def __init__(self):
+		pass
+
+# Action: fake share
+class FakeShare : Action
+	def __init__(self):
+		pass
+
+# Action: Vote
+class Vote : Action
+	def __init__(self):
+		pass		
---
+++ main.py
@@ -0,0 +1,18 @@
+
+
+
+def GetNewIp():
+	'''get new ip address'''
+	pass
+
+def FakeShare():
+	'''fake share action'''
+	pass
+
+def RefreshVerifyCode():
+	'''refresh verfy code'''
+	pass
+
+def Vote():
+	'''Now Vote'''
+	pass
